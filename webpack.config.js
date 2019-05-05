9fda4014ed8023c4cd58fc4102539b27237c3d98
hhz <860601963@qq.com>
2019-05-05 18:02:51 +0800

    test the plugin demo of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js'
-    }
+        filename: 'pluginTest.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'pluginTest.html',
+            template: 'src/index.html'
+        })
+    ]
 };
